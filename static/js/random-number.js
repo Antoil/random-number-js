dd8deaae54d8b8fd611d7b56c3637a42170696b1
Antoil <alinciano113@gmail.com>
2024-09-30 11:52:40 +0200

    inizio numero random

---
+++ static/js/random-number.js
@@ -0,0 +1,11 @@
+
+function generateRandomInteger(min, max) {
+
+}
+let firstName = null
+
+alert("Indovina il numero")
+
+while (firstName === null){
+    let firstName = prompt("Inserisci il tuo nome: ")
+}

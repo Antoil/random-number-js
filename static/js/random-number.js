6c40f401d0bae2e21444956329abf8f483be16c1
Antoil <alinciano113@gmail.com>
2024-10-14 15:00:33 +0200

    code evolution
    rimozione di prompt e alert

--- static/js/random-number.js
+++ static/js/random-number.js
@@ -1,42 +1,62 @@
 
-function generateRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
-}
 let max = 100
 let min = 1
-let randomNumber = generateRandomInteger(min, max)
-let indovinato = false
+let btn = document.querySelector("#guessBtn")
+let random = generateRandomInteger(min, max)
+let attempts = 1
+let maxAttempts = 5
 
+console.log(random)
 
-alert("Indovina il numero tra "+ min + " e " + max)
-
-for(i = 1; i < 6;  i++){
-    alert("Tentativo n°" + i)
-    
-        let number = prompt("Inserisci un numero compreso tra " + min + " e " + max)
-        number = parseInt(number)
-        while (number < 1 || number > 100){
-            let number = prompt("Numero sbagliato. Inserisci un numero compreso tra " + min + " e " + max)
-            number = parseInt(number)
-        }
-    while (number === isNaN){
-        let number = prompt("Inserisci un numero compreso tra " + min + " e " + max)
-        number = parseInt(number)
-        while (number < 1 || number > 100){
-            let number = prompt("Numero sbagliato. Inserisci un numero compreso tra " + min + " e " + max)
-            number = parseInt(number)
-        }
-    }
-    if (number > randomNumber) {
-        alert("Il numero inserito " + number + " è più grande")
-    } else if (number < randomNumber){
-        alert("Il numero inserito " + number + " è più piccolo")
-    } else {
-        alert("HAI INDOVINATO!!!!!!! al tentativo n°" + i , ". Il numero era: " + randomNumber)
-        indovinato = true
-    }
+function generateRandomInteger(min = 1, max = 100) {
+    return Math.floor(Math.random() * (max - min) + min)
 }
-alert("Tentativi terminati")
-if (indovinato === false) {
-    console.log("Sarai fotuntao un altra volta, il numero era: " + randomNumber)
+function handleClick(userfield){
+    let number // defaults to null
+    let userField = document.querySelector("#guess")
+    let indovinato = true
+
+    number = (userField.value).trim() 
+
+    if(number === null || isNaN(number))  {
+        console.log("Numero non valido")
+        return
+    }
+
+    number = parseInt(number)
+
+    if (attempts >= maxAttempts) {
+        console.log("Tentativi esauriti")
+        restart()
+        return
+    }
+
+    console.log("Tentativo numero " + attempts)
+
+    if(number < random){
+        console.log("Troppo Piccolo!")
+    }
+  
+    if(number > random){
+        console.log("Troppo Grande!")
+    }
+
+    if(number === random){
+        console.log("Hai vinto! Il numero era: " + random)
+        console.log("Riavvo gioco...")
+        restart()
+        return
+    }
+
+    attempts++
+
+
 }
+
+function restart() {
+            // generate another random number and display it subsequently
+            random = generateRandomInteger()
+            console.log("(" + random + ")")
+}
+
+btn.addEventListener("click", handleClick)
